90eea07d05e8360bd91a5539682a8717c1dacb45
Julian Vega <ingjulianvega@gmail.com>
2021-07-18 12:53:09 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasucompanion/configuration/ErrorCodeMessages.java
+++ src/main/java/ingjulianvega/ximic/msscasucompanion/configuration/ErrorCodeMessages.java
@@ -6,7 +6,7 @@ import lombok.experimental.UtilityClass;
 public class ErrorCodeMessages {
 
     public static final String COMPANION_NOT_FOUND_API_CODE = "401";
-    public static final String COMPANION_NOT_FOUND_ERROR = "BODY_PART_NOT_FOUND";
+    public static final String COMPANION_NOT_FOUND_ERROR = "COMPANION_NOT_FOUND";
     public static final String COMPANION_NOT_FOUND_MESSAGE = "No se encontró el acompañante";
     public static final String COMPANION_NOT_FOUND_SOLUTION = "Verifique los datos";
 
